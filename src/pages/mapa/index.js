f9ba75036517a4b9bfb181aff67cf24ab7ff4fae
siqueirad <danilo_siqueira@id.uff.br>
2023-04-30 22:47:24 -0300

    adicionando parte inicial do mapa e eslint com prettier

--- src/pages/mapa/index.js
+++ src/pages/mapa/index.js
@@ -1,3 +1,22 @@
+//import * as L from 'leaflet'
+import { useEffect } from "react";
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+
+const Map = dynamic(() => import("./teste"), {
+  loading: () => <p>A map is loading</p>,
+  ssr: false,
+});
+
 export default function Mapa() {
-  return (<div> Aqui é o mapa </div>)
+  return (
+    <main style={{ height: "100%" }}>
+      <div>
+        {" Aqui é o mapa "}
+        <div style={{ height: "580px", display: "grid" }}>
+          <Map />
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/pages/mapa/teste.js
@@ -0,0 +1,17 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+export default function Teste() {
+  const position = [51.505, -0.09]
+
+  return (<MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={position}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>)
+}
