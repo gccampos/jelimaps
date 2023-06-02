c75da676b2a6209f43fab0d20a11904b44a70055
Guilherme Campos <gcordeiro@Id.uff.br>
2023-06-02 14:01:20 -0300

    Adição de google analytcs ao projeto

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,8 @@
 import '@/styles/globals.css'
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return   <><GoogleAnalytics trackPageViews /><Component {...pageProps} /></>
 }
