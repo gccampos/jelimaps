16f5ea9710f9bcee8ac1308df2c9e6b500bc96aa
siqueirad <danilo_siqueira@id.uff.br>
2023-12-06 11:58:30 -0300

    Utilizando tag script do nextjs

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import {
   Experimental_CssVarsProvider as CssVarsProvider,
 } from "@mui/material/styles";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const theme = extendTheme({
   cssVarPrefix: "md-demo",
@@ -18,13 +19,11 @@ export default function Document() {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
         ></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag(\\"js\\", new Date());
-
-        gtag(\\"config\\", \\"${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}\\");`}
-        </script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </Script>
       </Head>
       <body>
         <CssVarsProvider theme={theme}>
