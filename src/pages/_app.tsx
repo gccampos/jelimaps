87b9d6dfee6e90605c847105483d02cc58488af7
siqueirad <danilo_siqueira@id.uff.br>
2023-12-06 11:40:41 -0300

    inserindo analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { GoogleAnalytics } from "nextjs-google-analytics";
 import CaixaDialogoProvider from "@/components/CaixaDialogo/CaixaDialogoProvider";
 import { BarraAlertaProvider } from "@/components/BarraAlerta/BarraAlertaProvider";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  console.log("teste", process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID);
   return (
     <>
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
       <CaixaDialogoProvider />
       <BarraAlertaProvider />
-      <Analytics />
     </>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ import {
   experimental_extendTheme as extendTheme,
   Experimental_CssVarsProvider as CssVarsProvider,
 } from "@mui/material/styles";
+import { Analytics } from "@vercel/analytics/react";
+import Script from "next/dist/client/script";
 
 const theme = extendTheme({
   cssVarPrefix: "md-demo",
@@ -13,10 +15,22 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');`}
+      </Script>
       <body>
         <CssVarsProvider theme={theme}>
           <Main />
         </CssVarsProvider>
+        <Analytics />
         <NextScript />
       </body>
     </Html>
