6a6197c13acfb6a9a9820e256d966d12b04433d2
siqueirad <danilo_siqueira@id.uff.br>
2024-05-07 02:41:27 -0300

    Apresentações Guidde da tela principal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,42 +71,8 @@ export default function Home() {
               suas principais funcionalidades, destacam-se:
             </Typography>
 
-            <Grid container spacing={2}>
-              <Grid container item xs={12} md={6} spacing={2}>
-                <Grid item xs={12}>
-                  Criação intuitiva de mapas: A interface amigável da JeliMaps
-                  permite aos usuários criar facilmente mapas personalizados em
-                  uma linha do tempo. É possível adicionar pontos de interesse,
-                  rotas, áreas e outros elementos geográficos de forma rápida e
-                  simples.
-                </Grid>
-                <Grid item xs={12}>
-                  Integração de imagens próprias: Os usuários têm a opção de
-                  utilizar suas próprias imagens georreferenciadas para
-                  enriquecer seus mapas. Isso permite a visualização de dados
-                  específicos e a criação de narrativas visuais impactantes.
-                </Grid>
-              </Grid>
-              <Grid container item xs={12} md={6} spacing={2}>
-                <Grid item xs={12}>
-                  Base OpenStreetMaps: A JeliMaps oferece suporte à integração
-                  de mapas da base de dados OpenStreetMaps. Os usuários podem
-                  aproveitar os recursos detalhados dessa fonte de informações
-                  aberta, permitindo uma representação precisa e atualizada do
-                  mundo.
-                </Grid>
-                <Grid item xs={12}>
-                  Linha do tempo interativa: A aplicação permite que os usuários
-                  definam datas e intervalos de tempo para a exibição de eventos
-                  e dados específicos em seus mapas. Com a funcionalidade da
-                  linha do tempo, é possível observar a evolução geográfica ao
-                  longo do tempo, analisar mudanças e descobrir padrões.
-                </Grid>
-              </Grid>
-            </Grid>
-
             <Stack
-              sx={{ pt: 4 }}
+              sx={{ py: 4 }}
               direction="row"
               spacing={2}
               justifyContent="center"
@@ -136,6 +102,111 @@ export default function Home() {
                 Veja exemplos
               </Button>
             </Stack>
+
+            <Grid container spacing={2}>
+              <Grid container item xs={12} md={6} spacing={2}>
+                <Grid item xs={12}>
+                  Criação intuitiva de mapas: A interface amigável da JeliMaps
+                  permite aos usuários criar facilmente mapas personalizados em
+                  uma linha do tempo. É possível adicionar pontos de interesse,
+                  rotas, áreas e outros elementos geográficos de forma rápida e
+                  simples.
+                  <div
+                    style={{ position: "relative", paddingBottom: "56.25%" }}
+                  >
+                    <iframe
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                      }}
+                      src="https://embed.app.guidde.com/playbooks/jRGUaXDj6KvBG8x1Sn1nQp"
+                      title="Criação intuitiva de mapas"
+                      // frameborder={0}
+                      // referrerpolicy="unsafe-url"
+                      // allowfullscreen="true"
+                      allow="clipboard-write"
+                      sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-forms allow-same-origin allow-presentation"
+                    ></iframe>
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                  Integração de imagens próprias: Os usuários têm a opção de
+                  utilizar suas próprias imagens georreferenciadas para
+                  enriquecer seus mapas. Isso permite a visualização de dados
+                  específicos e a criação de narrativas visuais impactantes.
+                  <div
+                    style={{ position: "relative", paddingBottom: "56.25%" }}
+                  >
+                    <iframe
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                      }}
+                      src="https://embed.app.guidde.com/playbooks/wujX3bgVH8wAeFrcdfcbtt"
+                      title="Integração de imagens próprias"
+                      allow="clipboard-write"
+                      sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-forms allow-same-origin allow-presentation"
+                    ></iframe>
+                  </div>
+                </Grid>
+              </Grid>
+              <Grid container item xs={12} md={6} spacing={2}>
+                <Grid item xs={12}>
+                  Base OpenStreetMaps: A JeliMaps oferece suporte à integração
+                  de mapas da base de dados OpenStreetMaps. Os usuários podem
+                  aproveitar os recursos detalhados dessa fonte de informações
+                  aberta, permitindo uma representação precisa e atualizada do
+                  mundo.
+                  <div
+                    style={{ position: "relative", paddingBottom: "56.25%" }}
+                  >
+                    <iframe
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                      }}
+                      src="https://embed.app.guidde.com/playbooks/x7UruBwgYCZnwhhDqZ8Yvi"
+                      title="Base OpenStreetMaps"
+                      allow="clipboard-write"
+                      sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-forms allow-same-origin allow-presentation"
+                    ></iframe>
+                  </div>
+                </Grid>
+                <Grid item xs={12}>
+                  Linha do tempo interativa: A aplicação permite que os usuários
+                  definam datas e intervalos de tempo para a exibição de eventos
+                  e dados específicos em seus mapas. Com a funcionalidade da
+                  linha do tempo, é possível observar a evolução geográfica ao
+                  longo do tempo, analisar mudanças e descobrir padrões.
+                  <div
+                    style={{ position: "relative", paddingBottom: "56.25%" }}
+                  >
+                    <iframe
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        position: "absolute",
+                        left: 0,
+                        top: 0,
+                      }}
+                      src="https://embed.app.guidde.com/playbooks/v1AfJB3y8KTiUGpVwcGXzw"
+                      title="Linha do tempo interativa"
+                      allow="clipboard-write"
+                      sandbox="allow-popups allow-popups-to-escape-sandbox allow-scripts allow-forms allow-same-origin allow-presentation"
+                    ></iframe>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
           </Container>
         </Box>
         <Container sx={{ py: 8 }} maxWidth="md" id="exemplos">
